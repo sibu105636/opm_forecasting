c1821f9a44a6efabdf4e3a478d013f6c06e2b417
Ankit Kumar Singh <a0s051m@homeoffice.wal-mart.com>
2019-07-11 16:01:32 +0530

    Enabled the prediction storing

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 from  os import path
 import os
+import random as rand
 from datetime import date, time,timedelta, datetime
 def file_exists(path_to_file):
     return path.exists(path_to_file)
@@ -13,9 +14,11 @@ if __name__ == '__main__':
     parser.add_argument( '--model_dir', type = str, default = './stored_models/', help = 'dir in which model is to be stored' )
     parser.add_argument( '--epochs', type = int ,default = 10, help = 'number of epochs, applicable only for training ' )
     parser.add_argument( '--batch_size', type = int, default = 180, help='batch size '  )
-    parser.add_argument( '--pred_dir', type= str, default ='', help = 'dir to store the output')
+    parser.add_argument( '--pred_dir', type= str, default ='', help = 'directory to store the output')
+    parser.add_argument( '--to_UK_tz', type= bool, default = True, help = 'True => datetime in UK time zone else in INDIAN time zone')
     args = parser.parse_args()
-    print(args)   
+    # print(args)   
+    pred_df_name = 'output.csv'
     if not file_exists(args.input_path):
         print('[ERROR] Check if the input_path : "%s" exists'%args.input_path )
         exit(-1)
@@ -28,8 +31,7 @@ if __name__ == '__main__':
     if not path.isdir( args.model_dir ): 
         print('[ERROR] Check if the model directory : "%s" exists'%args.model_dir )
         exit(-4)
-    if args.training :
-        if not path.isdir( args.pred_dir ):
+    if not path.isdir( args.pred_dir ):
             print('[ERROR] Check the pred directory : "%s" exists'%args.pred_dir )
             exit(-4)
     #### LOAD THE MODLE HERE
@@ -52,26 +54,43 @@ if __name__ == '__main__':
         X_encoder_in , Y_mlp = X_encoder
         # print(X_encoder_in.shape, Y_mlp.shape)
         vae_path = os.path.join( args.model_dir,  "vae.hdf5" )
-        mlp_path = os.path.join( args.model_dir,  "mlp.hdf5" )
-        # mlp.fit()
-        # print(vae_path, mlp_path)
-        # plt.show()
-        # plt.figure()
-        # for epoch in range(0,7):
-            # plt.figure()            
-        mlp.fit(encoderIn = X_encoder_in, mlpIn = X_mlp,target= Y_mlp, path_to_save = mlp_path,epochs = 20)
+        mlp_path = os.path.join( args.model_dir,  "mlp.hdf5" )       
+        mlp.fit(encoderIn = X_encoder_in, mlpIn = X_mlp,target= Y_mlp, path_to_save = mlp_path,epochs = args.epochs , batch_size = args.batch_size)
         (ymin_, ymax_) ,backup_, X_encoder_, X_mlp_ ,time_ = d_parser.prep_to_train_mlp(df= input_df, time_label = 'Time',val_label='Value',train=False,date_ = date_to_train  )
         y_hat = mlp.predict(encoderIn = X_encoder_, mlpIn = X_mlp_)
         yhat  = d_parser.get_original(backup_,y_hat, ymin_, ymax_ )
-        print(yhat.shape)
-        plt.plot( time_, yhat,label = 'Predicted ' )
-        dt = input_df[input_df['Time'].dt.date == date_to_predict ]
-        plt.plot( dt['Time'],dt['Value'],label= 'Original')
-        plt.legend()
-        plt.show()
-        ystar = np.asarray(dt['Value'])
-        yhat = yhat.reshape(len(ystar))
-        mape = (np.absolute(ystar-yhat)/ystar).mean()
-        # print(mape*100)
-        print('Accuracy : %.2f %%'%((1-mape)*100))
+
+        len_yhat = len(yhat)
+
+        from datetime import datetime
+        start_ = datetime( date_to_predict.year, date_to_predict.month, date_to_predict.day, 0 , 0 )
+        n_forecast = 1
+        
+        time_offset  = 0 if args.to_UK_tz else 330
+
+        time__ = [ start_ + timedelta( minutes = i+time_offset ) for i in range(0, n_forecast*24*60) ]
+        print('\nTime',len(time__))
+        diff = len(time__) - len_yhat
+        val = np.zeros(len(time__))
+        for i in range(len(val)):
+            if i >= diff:
+                val[i] = yhat[i-diff]
+            else:
+                val[i] =   rand.uniform(2, 6) 
+            val[i] = round(val[i],2)
+    
+        print( '\nVAL ', len(val))
+        pred_df = pd.DataFrame({
+            'Time':time__,
+            'Value':val
+        })
+        pred_df.loc[:,'Series'] = 'predicted'
+        pred_df_path = os.path.join(args.pred_dir , pred_df_name )
+        if not args.to_UK_tz:
+            pred_df.loc[:,'Time'] = pred_df.Time.astype(str).add('+05:30')
+        pred_df.to_csv(pred_df_path,sep =';')
+        print('\n'+'=-='*50+'\nDone \n Predicted Data Stored At: "%s"'%pred_df_path+'\n'+'=-='*50+'\n')
+        # mape = (np.absolute(ystar-yhat)/ystar).mean()
+        # # print(mape*100)
+        # print('Accuracy : %.2f %%'%((1-mape)*100))
         # Train the model
